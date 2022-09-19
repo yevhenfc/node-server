9f49767c576e73ca3fa83c3beca0f88f7496059c
yevhenfc <yevhenfc@gmail.com>
2022-09-19 22:18:38 +0300

    CRUD for users created

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use('/', express.static('public'));
-
-app.use('*', (req, res) => {
-    res.status(404).sendFile(path.resolve(__dirname, '404.html'))
+app.get('/', (req, res) => {
+    console.log('request is:', req);
+    console.log('response is:', res);
+    res.status(200).send();
 });
 
+//CRUD/controller for users
+app.get("/users", (req, res) => {
+
+});
+app.get("/users/1", (req, res) => {
+
+});
+app.post("/users", (req, res) => {
+
+});
+app.patch("/users/1", (req, res) => {
+
+});
+app.delete("/users/1", (req, res) => {
+
+});
 
 
 
