e6e98df0ca55f2f15fba0c85cc8863d5706141dd
yevhenfc <yevhenfc@gmail.com>
2022-09-19 22:02:21 +0300

    send pages with express

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('request: ', req)
-    res.status(200).end();
-});
+app.use('/', express.static('public'));
 
-app.post('/users', (req, res) => {
-    const {body} = req;
-    // const newUser = 
-    res.status(201).send(body);
-});
-
-app.get('/users/id', (req, res) => {
-    //
-    res.status(200).send();
+app.use('*', (req, res) => {
+    res.status(404).sendFile(path.resolve(__dirname, '404.html'))
 });
 
 
-// app.get('/users/id', (req, res) => {});
-// app.post('/users/', (req, res) => {});
-// app.patch('/users/id', (req, res) => {});
-// app.delete('/users/id', (req, res) => {});
+
 
 module.exports = app;
