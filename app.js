92885185376f3d445c0fb2da499dddef25b83d4e
yevhenfc <yevhenfc@gmail.com>
2022-09-19 22:43:20 +0300

    class Users created

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('request is:', req);
-    console.log('response is:', res);
-    res.status(200).send();
-});
+//db
+const usersDB = [
+    {
+        id: 1,
+        name: "Name1",
+        surname: "Surname1",
+        isMale: true,
+        age: 1
+    },
+    {
+        id: 2,
+        name: "Name2",
+        surname: "Surname2",
+        isMale: false,
+        age: 2
+    },
+    {
+        id: 3,
+        name: "Name3",
+        surname: "Surname3",
+        isMale: true,
+        age: 3
+    },
+    {
+        id: 4,
+        name: "Name4",
+        surname: "Surname4",
+        isMale: false,
+        age: 4
+    }
+];
+
+class Users{
+    constructor(users){
+        this.users = [...users];
+        this.count = users.length;
+    }
+    createUser(user){}
+    getUserById(id){}
+    getllUsers(){}
+    updateUser(id, newInfo){}
+    deleteUser(id){}
+}
+const usersInstance = new Users(usersDB);
+
 
 //CRUD/controller for users
+//get all users
 app.get("/users", (req, res) => {
-
+    const data = usersInstance.getllUsers();
+    res.status(200).send(data);
 });
 app.get("/users/1", (req, res) => {
 
