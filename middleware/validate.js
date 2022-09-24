fc91c340aeac89b49b497e23392d3fe9b51879cd
yevhenfc <yevhenfc@gmail.com>
2022-09-24 17:47:29 +0300

    async validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,22 +1,21 @@
 const {serCreateSchema, userUpdateSchema} = require('../utils/validationSchemas');
 
-module.exports.validateUserCreate = (req, res, next) => {
+module.exports.validateUserCreate = async (req, res, next) => {
     const {body} = req;
-    userCreateSchema.validate(body).then((validatedUser) => {
+    try {
+        const validatedUser = await userCreateSchema.validate(body);        
         req.body = validatedUser;
         next();
-    }).catch((error) => {
-        res.status(400).send(error);
-    })
+    }
+    catch(error){res.status(400).send(error)}
 }
 
-
-module.exports.validateUserUpdate = (req, res, next) => {
+module.exports.validateUserUpdate = async (req, res, next) => {
     const {body} = req;
-    userUpdateSchema.validate(body).then((validatedUser) => {
+    try {
+        const validatedUser = await userUpdateSchema.validate(body);        
         req.body = validatedUser;
         next();
-    }).catch((error) => {
-        res.status(400).send(error);
-    })
-}
+    }
+    catch(error){res.status(400).send(error)}
+ }
